365ba5eeb92275ae8cc6f21076045ce089c7cdbd
allek6496 <allek6496@wrdsb.ca>
2020-01-19 00:45:35 -0500

    ;lkjsadckmepiruhcn;lfgbdh vl;clvfbnwqx
    qd s,cz:KJv ckp sj, m,fkchvnsal;kujfc

--- starterhacks-react-app/src/Pages/Chat.js
+++ starterhacks-react-app/src/Pages/Chat.js
@@ -6,42 +6,50 @@ import fire from '../Fire';
 class ChatBox extends React.Component {
     constructor (props) {
         super(props);
+        this.send = this.send.bind(this);
         this.state = {
             otherUid: "5pGqew3j3thWoXiNsrNphskRF2o2",
-            incoming: [],
-            outgoing: []
+            uid: "",
+            messages: []
         };
+    }
+
+    send(e) {
+        e.preventDefault();
+        var message = document.getElementsByClassName('message')[0].value;
+
+        fire.database().ref("users/" + this.state.uid + "/chatlog").set({"message": message, "to": this.state.otherUid});
     }
 
     render() {
         fire.auth().onAuthStateChanged((user) => {
             console.log(user);
             if (user) {
-              fire.database().ref('starter-ba2c5/users/' + user.uid + "/sent").on('child_added', function(childSnapshot, prevChildKey) {
-            if (childSnapshot.child("/to").val() == this.state.otherUid){
-                this.setState(prevState => {
-                    prevState.outgoing.push(childSnapshot.child("message"));
+                this.setState({uid: user.uid.toString()})
+                fire.database().ref('starter-ba2c5/users/' + user.uid + "/chatlog").on('child_added', function(childSnapshot, prevChildKey) {
+                    if (childSnapshot.child("/to").val() == this.state.otherUid){
+                        this.setState(prevState => {
+                            prevState.messages.push([user.uid, childSnapshot.child("message")]);
+                        });
+                        // this.state.outgoing.push(childSnapshot.child("message"));
+                    }
                 });
-                // this.state.outgoing.push(childSnapshot.child("message"));
+                fire.database().ref('starter-ba2c5/users/' + user.uid + "/chatlog").on("child_added", function(childSnapshot, prevChildKey) {
+                    if (childSnapshot.child("/to").val() == user.uid){
+                        this.setState(prevState => {
+                            prevState.messages.push([user.uid, childSnapshot.child("message")]);
+                        });
+                        // this.state.incoming.push(childSnapshot.child("message"));
+                    }
+                });
             }
         });
-        fire.database().ref('starter-ba2c5/users/' + user.uid + "/sent").on("child_added", function(childSnapshot, prevChildKey) {
-            if (childSnapshot.child("/to").val() == user.uid){
-                this.setState(prevState => {
-                    prevState.incoming.push(childSnapshot.child("message"));
-                });
-                // this.state.incoming.push(childSnapshot.child("message"));
-            }
-        });
-            }
-          });
           
-        console.log(this.state.incoming, "incoming");
-        console.log(this.state.outoing, "outgoing");
+        console.log(this.state.messages, "messages");
         return(
             <div>
-                <input class="field__input a-field__input" placeholder="Enter Your Password..." required/>
-                <button class="button" onClick={this.message}>Submit</button>
+                <input class="message" placeholder="Enter Your Message..." required/>
+                <button class="button" onClick={this.send}>send</button>
             </div>
         );
     }
--- starterhacks-react-app/src/Pages/Login.js
+++ starterhacks-react-app/src/Pages/Login.js
@@ -4,98 +4,49 @@ import {Link} from 'react-router-dom'
 import fire from '../Fire';
 
 class Login extends React.Component{
-    constructor (props) {
+    constructor(props) {
         super(props);
-        this.send = this.send.bind(this);
+        this.login = this.login.bind(this);
         this.state = {
-            otherUid: "5pGqew3j3thWoXiNsrNphskRF2o2",
-            uid: "",
-            messages: []
+          email: '',
+          password: ''
         };
-    }
+      }
 
-    send(e) {
+    login(e) {
         e.preventDefault();
-        var message = document.getElementsByClassName('message')[0].value;
-
-        fire.database().ref("users/" + this.state.uid + "/chatlog").set({"message": message, "to": this.state.otherUid});
+        var Email = document.getElementsByClassName('field__input a-field__input')[0].value;
+        var Password = document.getElementsByClassName('field__input a-field__input')[0].value;
+        fire.auth().signInWithEmailAndPassword(Email, Password).then((u)=>{
+        }).catch((error) => {
+            console.log(error);
+          });
     }
-
-    render() {
-        fire.auth().onAuthStateChanged((user) => {
-            console.log(user);
-            if (user) {
-                this.setState({uid: user.uid.toString()})
-                fire.database().ref('starter-ba2c5/users/' + user.uid + "/chatlog").on('child_added', function(childSnapshot, prevChildKey) {
-                    if (childSnapshot.child("/to").val() == this.state.otherUid){
-                        this.setState(prevState => {
-                            prevState.messages.push([user.uid, childSnapshot.child("message")]);
-                        });
-                        // this.state.outgoing.push(childSnapshot.child("message"));
-                    }
-                });
-                fire.database().ref('starter-ba2c5/users/' + user.uid + "/chatlog").on("child_added", function(childSnapshot, prevChildKey) {
-                    if (childSnapshot.child("/to").val() == user.uid){
-                        this.setState(prevState => {
-                            prevState.messages.push([user.uid, childSnapshot.child("message")]);
-                        });
-                        // this.state.incoming.push(childSnapshot.child("message"));
-                    }
-                });
-            }
-        });
-          
-        console.log(this.state.messages, "messages");
+    render(){
         return(
-            <div>
-                <input class="message" placeholder="Enter Your Message..." required/>
-                <button class="button" onClick={this.send}>send</button>
+            <div className="login-div">
+                <div className="login-div-content">
+                    <h1 className="login-title">Toast</h1>
+                    <label class="field a-field a-field_a1">
+                        <input class="field__input a-field__input" placeholder="Enter Your Email..." required/>
+                        <span class="a-field__label-wrap">
+                        <span class="a-field__label">Email</span>
+                        </span>
+                    </label>
+                    <label class="field a-field a-field_a1">
+                    <input class="field__input a-field__input" placeholder="Enter Your Password..." required/>
+                    <span class="a-field__label-wrap">
+                    <span class="a-field__label">Password</span>
+                    </span>
+                </label>
+                    <Link to="/register" style={{ textDecoration: 'none', color: 'white', fontSize: '15px' }} className="register-link-button"> Register</Link>
+                    <div class="wrap">
+                        <button class="button" onClick={this.login}>Submit</button>
+                    </div>
+                </div>
             </div>
-        );
+        )
     }
-    // constructor(props) {
-    //     super(props);
-    //     this.login = this.login.bind(this);
-    //     this.state = {
-    //       email: '',
-    //       password: ''
-    //     };
-    //   }
-
-    // login(e) {
-    //     e.preventDefault();
-    //     var Email = document.getElementsByClassName('field__input a-field__input')[0].value;
-    //     var Password = document.getElementsByClassName('field__input a-field__input')[0].value;
-    //     fire.auth().signInWithEmailAndPassword(Email, Password).then((u)=>{
-    //     }).catch((error) => {
-    //         console.log(error);
-    //       });
-    // }
-    // render(){
-    //     return(
-    //         <div className="login-div">
-    //             <div className="login-div-content">
-    //                 <h1 className="login-title">Toast</h1>
-    //                 <label class="field a-field a-field_a1">
-    //                     <input class="field__input a-field__input" placeholder="Enter Your Email..." required/>
-    //                     <span class="a-field__label-wrap">
-    //                     <span class="a-field__label">Email</span>
-    //                     </span>
-    //                 </label>
-    //                 <label class="field a-field a-field_a1">
-    //                 <input class="field__input a-field__input" placeholder="Enter Your Password..." required/>
-    //                 <span class="a-field__label-wrap">
-    //                 <span class="a-field__label">Password</span>
-    //                 </span>
-    //             </label>
-    //                 <Link to="/register" style={{ textDecoration: 'none', color: 'white', fontSize: '15px' }} className="register-link-button"> Register</Link>
-    //                 <div class="wrap">
-    //                     <button class="button" onClick={this.login}>Submit</button>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     )
-    // }
 }
 
 export default Login
