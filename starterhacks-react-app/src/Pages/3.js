17bc3fe55fc5ef30284a1a2c14cc9241143059d7
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-01-19 06:30:56 -0500

    cs

--- starterhacks-react-app/src/Pages/3.js
+++ starterhacks-react-app/src/Pages/3.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import './3.css'
 
 class three extends React.Component{
     render(){
         return(
-            <div></div>
+            <div className="three-div">
+                <div className="three-text">Heres Your Match</div>
+                
+            </div>
         )
     }
 }
