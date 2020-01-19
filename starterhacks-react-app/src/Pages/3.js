979a61da1858eaa03d3bf98b211d67e99e643c55
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-01-19 07:01:27 -0500

    nig

--- starterhacks-react-app/src/Pages/3.js
+++ starterhacks-react-app/src/Pages/3.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import './3.css'
+import ThreeCard from './3Card'
 
 class three extends React.Component{
     render(){
         return(
             <div className="three-div">
-                <div className="three-text">Heres Your Match</div>
-                
+                <div className="three-text">Here's Your Matches</div>
+                <div className="therapist-div">
+                    <ThreeCard></ThreeCard>
+                    <ThreeCard></ThreeCard>
+                    <ThreeCard></ThreeCard>
+
+                </div>
             </div>
         )
     }
--- starterhacks-react-app/src/Pages/3Card.js
+++ starterhacks-react-app/src/Pages/3Card.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './3Card.css';
+
 
 class ThreeCard extends React.Component{
     render(){
         return(
-            <div></div>
+            <div className="therapist-card">
+                <div className="therapist-pfp"></div>
+                <div className="therapist-name">Daniel Yu</div>
+                <div className="therapist-bio">I love long walks on the beach with my lovely doggos</div>
+                <div class="wrap3">
+                        <button class="button3" onClick={this.login}>Chat</button>
+                    </div>
+
+            </div>
         )
     }
 }
